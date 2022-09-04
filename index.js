927c5e0b47e57f4aeae169ac5d46fdb94e25fac0
superb-shan <shankar.k2021csec@sece.ac.in>
2022-09-04 19:41:57 +0530

    MongoDB Connection Established

--- index.js
+++ index.js
@@ -2,10 +2,19 @@
 //express installed
 // Put npm run start or npm run dev to start the server
 // Press Ctrl+C to end the server in terminal
+// mongoose installed
+//dotenv installed
 
 const express = require("express");
+const dotenv = require("dotenv"); // requiring .env
+const DbConnection = require("./DBConnection"); //Database connection
+
+dotenv.config();//calling / using .env
+
 const PORT = 8081;
 const app = express();
+
+DbConnection(); // calling DbConnection
 
 app.use(express.json());
 
